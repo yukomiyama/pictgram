d5c9ccb995e257195e714c8458e27451e510499c
Yu Komiyama <ec2-user@ip-172-31-34-130.us-east-2.compute.internal>
2019-07-08 04:22:14 +0000

    finish login and logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,34 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to root_path, success: 'ログインに成功しました'
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render :new
+    end
+  end
+  
+  def destroy
+    log_out
+    redirect_to root_path, info: 'ログアウトしました'
+  end
+  
+  private
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+  
+  def session_params
+    params.require(:session).permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 15 }
   validates :email, presence: true, format: { with:  /\A^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$\z/ }
-  validates :password, format: { with: /\A^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z\-]{8,32}$\z/,
-  message: "大文字英数を1つ以上含む英数字8文字以上32文字以内で設定してください" }
+  validates :password, format: { with: /\A^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z\-]{8,32}$\z/ }
   has_secure_password
 end
